56586a97a97f9558b606e5b6ddf8b4b091c99585
Lucas Valdez <lnvaldez24@gmail.com>
2024-12-04 11:13:25 -0300

    fix: use get method for logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/user.controller");
+const { register, login, logout } = require("../controllers/user.controller");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 
 router.get("/login", (req, res) => {
   res.render("pages/login");
